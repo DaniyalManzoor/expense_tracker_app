d7c7d96b702872705586d80431d87bf9ec896fe6
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-06-26 16:58:15 +0500

    update add Transaction

--- src/components/addTransaction.jsx
+++ src/components/addTransaction.jsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
   return (
     <>
       <h3>Add new Transaction</h3>
       <form action="">
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" placeholder="Enter Text..." />
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter Text..."
+          />
         </div>
         <div className="form-control">
           <label htmlFor="text">
             Amount <br /> (negative - expense And Positive - income
           </label>
-          <input type="number" placeholder="Enter amount..." />
+          <input
+            type="number"
+            value={amount}
+            onChange={() => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
         </div>
         <button className="btn">Add Transaction</button>
       </form>
