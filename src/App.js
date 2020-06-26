32da7d26a6eb20a88738c2ec266b24833dc9399e
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-06-26 13:55:03 +0500

    add Childs component in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import Header from "./components/header";
+import Balance from "./components/balance";
+import IncomeExpense from "./components/incomeExpenses";
+import TransactionList from "./components/transactionList";
 import "./App.css";
 
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <React.Fragment>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default App;
